da30927b133ec8853fac1f424037205d04bb9058
Usama Syed <usama.syed@lynx.italy>
2024-03-28 18:15:05 +0100

    First Java Lesson 28/03

--- Excercises/src/com/github/training/excercises/Excercises.java
+++ Excercises/src/com/github/training/excercises/Excercises.java
@@ -1,9 +1,50 @@
 package com.github.training.excercises;
 
+import java.util.Scanner;
+
+import com.github.training.excercises.bean.Cane;
+
 public class Excercises {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, Java app");
+		
+		example();
+		esercizio1();
+		
 	}
-
+	
+	private static void example() {
+		Scanner input = new Scanner(System.in);
+		int number = 0;
+		String result = null;
+		Cane myDog = new Cane();
+		int[] numbers = new int[5];
+		
+		System.out.print("Digitare un numero intero ");
+		number = input.nextInt();
+		
+		if (number % 2 == 0) {
+			result = "Il numero digitato " + number + " è un numero pari";
+		} else {
+			result = "Il numero digitato" + number + " un numero dispari";
+		}
+		
+		System.out.println("Hello World, Java app");
+		System.out.println("Number: " + number);
+		System.out.println(result);
+		
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print("Digitare un numero intero ");
+			number = input.nextInt();
+			numbers[i] = number;
+			System.out.println("Numero " + (i+1) + ": " + numbers[i]);
+		}
+		
+		input.close();
+		System.out.print("Fine Programma");
+	}
+	
+	private static void esercizio1() {
+		//TODO 
+	}
 }
---
+++ Excercises/src/com/github/training/excercises/bean/Cane.java
@@ -0,0 +1,5 @@
+package com.github.training.excercises.bean;
+
+public class Cane {
+
+}
